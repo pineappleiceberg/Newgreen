e89f95ba9977d0b5216b8d99c5065ce0e908a84e
pineappleiceberg <amrish@waveindustries.com>
2023-11-06 12:01:48 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,4 +26,40 @@ async def root():
 
 
 
+@app.post('/users/', response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
 
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered.")
+
+    return crud.create_user(db=db, user=user)
+
+
+@app.get('/users/', response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+
+    return users
+
+
+@app.get('/users/{user_id}', response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found.")
+
+    return db_user
+
+
+@app.post('/users/{user_id}/budgetitems/', response_model=schemas.BudgetItem)
+def create_budget_item(user_id: int, budget_item: schemas.BudgetCreate, db: Session = Depends(get_db)):
+    return crud.create_user_budget_item(db=db, budget_items=budget_item, user_id=user_id)
+
+
+@app.get('/budgetitems/', response_model=List[schemas.BudgetItem])
+def read_budget_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    budget_items = crud.get_budget_items(db, skip=skip, limit=limit)
+
+    return budget_items
