81a6c9860903b47368e0fe5905c36e1a66d974b2
pineappleiceberg <80722277+pineappleiceberg@users.noreply.github.com>
2023-11-05 21:29:28 -0600

    Crud functions I forgot to upload earlier

---
+++ backend/crud.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import Session
+import hashlib
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = hashlib.sha512(user.password.encode()).hexdigest()
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+def get_home_works(db: Session, skip: int = 0, limit: int = 1000):
+    return db.query(models.BudgetEntry).offset(skip).limit(limit).all()
+
+
+def create_user_home_work(db: Session, budget_items: schemas.BudgetItem, user_id: int):
+    db_budget_items = models.BudgetEntry(**budget_items.dict(), owner_id=user_id)
+    db.add(db_budget_items)
+    db.commit()
+    db.refresh(db_budget_items)
+
+    return db_budget_items
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
