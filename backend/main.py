05e135592f78dabae2f53aa2bbe54a8bcf6e6dbe
pineappleiceberg <80722277+pineappleiceberg@users.noreply.github.com>
2023-11-26 19:13:40 -0600

    Backend integration
    Registration, log in, and calendar have functional integration but they may not be in their final state. A token system was added for authentication. The secret key is fixed and the same for each token so this is not secure for production. The calendar page was reworked and now has a place to add new entries and it autopopulates with existing items. If a tally or date would be desirable, we can discuss that at our next team meeting.

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,73 @@
 from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, FastAPI, HTTPException, Request, Depends, status
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-
-
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from . import crud, models, schemas
 from .database import SessionLocal, engine
-
 models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Dependency
+
+
+
+SECRET_KEY = "0aaa9dec23dcb5e0a78f6ab35e6301b2028cd8c34b270c4e30a7bd9d3ee08984"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+def get_user_id_from_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("user_id")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        return email
+    except JWTError:
+        raise credentials_exception
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -51,14 +105,24 @@ class LoginRequest(BaseModel):
     email: str
     password: str
 
-@app.post("/login/", response_model=schemas.User)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+@app.post("/login/", response_model=Token)
 def login(request: Request, req_body: LoginRequest, db: Session = Depends(get_db)):
     db_user = crud.login_user(db, email=req_body.email, password=req_body.password)
 
-    if db_user != None:
-        return db_user
+    if db_user is not None:
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": db_user.email, "user_id": db_user.id},
+            expires_delta=access_token_expires
+        )
+        return {"access_token": access_token, "token_type": "bearer"}
     else:
-        raise HTTPException(status_code=400, detail="No account with those credentials could be authenticataed")
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
 
 
 @app.get('/users/{user_id}', response_model=schemas.User)
@@ -71,25 +135,19 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post('/users/{user_id}/budgetitems/', response_model=schemas.BudgetItem)
-def create_budget_item(user_id: int, budget_item: schemas.BudgetCreate, db: Session = Depends(get_db)):
+@app.post('/createbudgetitem/', response_model=schemas.BudgetItem)
+def create_budget_item(budget_item: schemas.BudgetCreate, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    user_id = get_user_id_from_token(token)
     return crud.create_user_budget_item(db=db, budget_items=budget_item, user_id=user_id)
-
-
 @app.get('/budgetitems/', response_model=List[schemas.BudgetItem])
-def read_budget_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_budget_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user: str = Depends(get_current_user)):
     budget_items = crud.get_budget_items(db, skip=skip, limit=limit)
-
     return budget_items
 
+@app.get('/userbudgetitems/', response_model=List[schemas.BudgetItem], tags=["BudgetItems"])
+def get_budget_items_for_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    user_id = get_user_id_from_token(token)
 
-@app.get('/users/{user_id}/getbudgetitems/', response_model=List[schemas.BudgetItem], tags=["BudgetItems"])
-def get_budget_items_for_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found.")
 
     budget_items = crud.get_budget_items_for_user(db, user_id=user_id)
-
     return budget_items
